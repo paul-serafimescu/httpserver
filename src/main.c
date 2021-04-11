f58dd7fb20a39196f505a5b729e1c78b42449b2e
PaulSera1 <paulserafimescu@gmail.com>
2021-04-10 19:20:50 -0700

    boredom

--- src/main.c
+++ src/main.c
@@ -1,10 +1,12 @@
 #include "server.h"
+#include "utils.h"
 
 #define PORT 8000
 #define MAX_CONNECTIONS 10
 
 int main(int argc, char **argv) {
 
+  // listdir(".");
   http_server *server = create_server(PORT, MAX_CONNECTIONS);
   run(server);
 
---
+++ src/utils.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <dirent.h>
+#include <string.h>
+#include "utils.h"
+
+int listdir(const char *path)
+{
+  char new_path[300]; // i think this is reasonable
+  struct dirent *dp;
+  DIR *dir = opendir(path);
+
+  if (!dir) {
+    return -1;
+  }
+
+  while ((dp = readdir(dir)) != NULL) {
+    if (dp->d_name[0] != '.') {
+      printf("%s\n", dp->d_name);
+
+      strcpy(new_path, path);
+      strcat(new_path, "/");
+      strcat(new_path, dp->d_name);
+
+      listdir(new_path);
+    }
+  }
+
+  if (closedir(dir) < 0) {
+    return -1;
+  }
+
+  return 0;
+}
